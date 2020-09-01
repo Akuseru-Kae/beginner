259de147ea8ab3e6b1904f9e28cec7f3588de79c
Kaemeister <63862292+kaemeister@users.noreply.github.com>
2020-09-02 01:06:39 +0600

    Update even or odd number.py

--- even or odd number.py
+++ even or odd number.py
@@ -1,5 +1,6 @@
-x =float(input('A integer number= '))
-if(x%2 == 0):
-    print('Even number')
-else:
-    print('Odd number')    
+def even_checker(num):
+    if num%2==0:
+        print('Even!!')
+    else:
+        print('Odd!!')
+even_checker(5) 
